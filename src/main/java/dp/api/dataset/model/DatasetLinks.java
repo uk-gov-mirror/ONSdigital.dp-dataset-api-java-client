3c01b28030b372cbbf042931bf26608696da1414
Carl Hembrough <carl@bitwaves.co.uk>
2018-03-06 13:01:46 +0000

    Add taxonomy link to dataset model

--- src/main/java/dp/api/dataset/model/DatasetLinks.java
+++ src/main/java/dp/api/dataset/model/DatasetLinks.java
@@ -12,11 +12,22 @@ public class DatasetLinks {
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private Link self;
 
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Link taxonomy;
+
     public Link getSelf() {
         return self;
     }
 
     public void setSelf(Link self) {
         this.self = self;
+    }
+
+    public Link getTaxonomy() {
+        return taxonomy;
+    }
+
+    public void setTaxonomy(Link taxonomy) {
+        this.taxonomy = taxonomy;
     }
 }
